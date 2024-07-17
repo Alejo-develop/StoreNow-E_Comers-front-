a6bb569ce6f8bb73f4d4db9c0fd5e397d0e8e2ca
Alejo-develop <yeyito872@gmail.com>
2024-07-16 21:28:18 -0500

    Second commit

---
+++ components/navbar.tsx
@@ -0,0 +1,5 @@
+export default function NavbarComponent(){
+    return (
+      <h1>Hola desde navbar</h1>
+    )
+  }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,12 +9,15 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'dark-gradient': 'linear-gradient(rgba(72, 24, 120), rgba(0, 0, 0, 0))',
+      },
+      colors: {
+        darkBg: 'var(--dark-gradient)',
       },
     },
   },
   plugins: [],
 };
+
 export default config;
+
